af9cc267054c99ec527a1e7c67b697db570f1f44
Meeaviishek <155694061+Meeaviishek@users.noreply.github.com>
2024-08-13 04:53:16 +0530

    first change commit  dchhcgghhgccjjc

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server:{
+    proxy:{
+      'views':'https://portfolio-backend-delta-ochre.vercel.app'
+    }
+  }
   plugins: [react()],
 })
